9ab30fe5955c56d90d5434823e0416d40b923aa0
Jim Thomson <jthomson@retsd.mb.ca>
2020-03-21 16:20:52 -0500

    Sage Game Character
    Basic AI Character

---
+++ SAGE GAME CHARACTER/Assets/AIAttacks.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIAttacks : MonoBehaviour
+{
+    private Animator animator;
+    private NavMeshAgent agent;
+    private bool isAttacking;
+    public float attackDistance;
+
+    void Awake ()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance <= attackDistance)
+            if (!isAttacking)
+            {
+                StartCoroutine("Attack", 1.5f);
+            }
+    }
+
+    IEnumerator Attack(float WaitTime)
+    {
+        isAttacking = true;
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(WaitTime);
+        isAttacking = false;
+    }
+}
---
+++ SAGE GAME CHARACTER/Assets/TagYourIt.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class TagYourIt : MonoBehaviour {
+
+    public GameObject Player;
+    private Animator anim;
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        anim = Player.GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollsionEnter(Collision collision)
+    {
+        if(collision.gameObject == Player){
+            anim.speed = 0;
+            agent.speed = 0;
+            Invoke("ResetSpeed", 2.0f);
+        }
+    }
+
+    void ResetSpeed()
+    {
+        anim.speed = 1;
+        agent.speed = 0.5f ;
+    }
+}
